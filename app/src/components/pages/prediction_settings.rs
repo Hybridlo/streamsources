162a80e1de1e2f69b6d26f01946cafc8d5c6b298
Павел <pashanoy@gmail.com>
2022-08-07 19:55:25 +0300

    make a shared struct for source options

--- app/src/components/pages/prediction_settings.rs
+++ app/src/components/pages/prediction_settings.rs
@@ -4,9 +4,11 @@ use twitch_sources_rework::front_common::source_options::NormalSourceOptions;
 
 #[function_component(PredictionsSettings)]
 pub fn predictions_settings() -> Html {
+    let options: UseStateHandle<NormalSourceOptions> = use_state(Default::default);
+
     html! {
         <>
-            { NormalSourceOptions::to_html() }
+            { NormalSourceOptions::to_html(&options) }
         </>
     }
 }
--- src/front_common/source_options.rs
+++ src/front_common/source_options.rs
@@ -1,31 +1,65 @@
-use yew::prelude::{Html, html};
+use yew::{prelude::{Html, html, UseStateHandle}};
 
+#[derive(Clone, Copy)]
 pub enum SourceColor {
     White,
     Black
 }
 
+impl Default for SourceColor {
+    fn default() -> Self {
+        Self::Black
+    }
+}
+
+#[derive(Default)]
 pub struct NormalSourceOptions {
     pub color: SourceColor,
     pub is_expanded: bool
 }
 
 impl NormalSourceOptions {
-    fn render_color_option(color: &SourceColor) -> Html {
+    pub fn with_color(&self, color: SourceColor) -> Self {
+        Self {
+            color,
+            is_expanded: self.is_expanded
+        }
+    }
+
+    pub fn with_is_expanded(&self, is_expanded: bool) -> Self {
+        Self {
+            color: self.color,
+            is_expanded
+        }
+    }
+
+    fn render_color_option(state: &UseStateHandle<NormalSourceOptions>, color: SourceColor) -> Html {
         // makes sure to fill all options if there'll be more
         match color {
             SourceColor::White => html! {
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="textColor" id="textColorWhite" />
-                    <label class="form-check-label">
+                    <input
+                        class="form-check-input" type="radio" name="textColor" id="textColorWhite"
+                        onclick={
+                                let state = state.clone();
+                                move |_| state.set((*state).with_color(color))
+                            }
+                        />
+                        <label class="form-check-label" for="textColorWhite">
                         { "While" }
                     </label>
                 </div>
             },
             SourceColor::Black => html! {
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="textColor" id="textColorBlack" checked=true />
-                    <label class="form-check-label">
+                    <input
+                        class="form-check-input" type="radio" name="textColor" id="textColorBlack" checked=true
+                        onclick={
+                                let state = state.clone();
+                                move |_| state.set((*state).with_color(color))
+                            }
+                        />
+                        <label class="form-check-label" for="textColorBlack">
                         { "Black" }
                     </label>
                 </div>
@@ -33,25 +67,31 @@ impl NormalSourceOptions {
         }
     }
 
-    fn render_color_options() -> Html {
+    fn render_color_options(state: &UseStateHandle<NormalSourceOptions>) -> Html {
         html! {
             <div class="col-6">
                 <div class="p-3 border border-dark border-2 h-100">
                     <h5 class="text-center">{ "Text color" }</h5>
-                    { NormalSourceOptions::render_color_option(&SourceColor::Black) }
-                    { NormalSourceOptions::render_color_option(&SourceColor::White) }
+                    { NormalSourceOptions::render_color_option(state, SourceColor::Black) }
+                    { NormalSourceOptions::render_color_option(state, SourceColor::White) }
                 </div>
             </div>
         }
     }
 
-    fn render_expand_option(is_expanded: bool) -> Html {
+    fn render_expand_option(state: &UseStateHandle<NormalSourceOptions>, is_expanded: bool) -> Html {
         match is_expanded {
             true => html! {
                 <div class="col-12">
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="expandedOption" id="expandedOptionTrue" />
-                        <label class="form-check-label">
+                        <input
+                            class="form-check-input" type="radio" name="expandedOption" id="expandedOptionTrue"
+                            onclick={
+                                let state = state.clone();
+                                move |_| state.set((*state).with_is_expanded(is_expanded))
+                            }
+                        />
+                            <label class="form-check-label" for="expandedOptionTrue">
                             { "Maximized" }
                         </label>
                     </div>
@@ -60,38 +100,44 @@ impl NormalSourceOptions {
             false => html! {
                 <div class="col-12">
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="expandedOption" id="expandedOptionFalse" checked=true />
-                        <label class="form-check-label">
+                        <input
+                            class="form-check-input" type="radio" name="expandedOption" id="expandedOptionFalse" checked=true
+                            onclick={
+                                let state = state.clone();
+                                move |_| state.set((*state).with_is_expanded(is_expanded))
+                            }
+                        />
+                        <label class="form-check-label" for="expandedOptionFalse">
                             { "Minimized" }
                         </label>
-                    </div>
+                        </div>
                 </div>
             },
         }
     }
 
-    fn render_expand_options() -> Html {
+    fn render_expand_options(state: &UseStateHandle<NormalSourceOptions>) -> Html {
         html! {
             <div class="col-6">
                 <div class="p-3 border border-dark border-2 h-100">
                     <h5 class="text-center">{ "Size options" }</h5>
                     <div class="row">
-                        { NormalSourceOptions::render_expand_option(false) }
-                        { NormalSourceOptions::render_expand_option(true) }
+                        { NormalSourceOptions::render_expand_option(state, false) }
+                        { NormalSourceOptions::render_expand_option(state, true) }
                     </div>
                 </div>
             </div>
         }
     }
     
-    pub fn to_html() -> Html {
+    pub fn to_html(state: &UseStateHandle<NormalSourceOptions>) -> Html {
         html! {
             <>
                 <h4 class="text-center">{ "Settings" }</h4>
                 <div class="container mb-3">
                     <div class="row gx-3">
-                        { NormalSourceOptions::render_color_options() }
-                        { NormalSourceOptions::render_expand_options() }
+                        { NormalSourceOptions::render_color_options(state) }
+                        { NormalSourceOptions::render_expand_options(state) }
                     </div>
                 </div>
             </>
